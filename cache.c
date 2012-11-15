54448cb5220e96d98e2e1ec6c433533fd538109b
Tim van der Molen <tbvdm@xs4all.nl>
2012-11-15 20:05:12 +0100

    Read the metadata-cache file with fread() instead of mmap(). There is no good reason to use mmap(). Furthermore, modest testing shows that the fread() implementation is a bit faster than the mmap() one.

--- cache.c
+++ cache.c
@@ -14,17 +14,11 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-
-#include <errno.h>
-#include <fcntl.h>
 #include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "siren.h"
 
@@ -34,6 +28,7 @@
 #include "compat/tree.h"
 #endif
 
+#define CACHE_BUFSIZE	4096
 #define CACHE_VERSION	0
 
 struct cache_entry {
@@ -54,16 +49,21 @@ static int		 cache_cmp_entry(struct cache_entry *,
 			    struct cache_entry *);
 static void		 cache_free_entry(struct cache_entry *);
 static int		 cache_read_file(void);
-static int		 cache_read_number(char **, char *, unsigned int *);
-static int		 cache_read_string(char **, char *, char **);
+static int		 cache_read_number(unsigned int *);
+static int		 cache_read_string(char **);
 static void		 cache_remove_entry(struct cache_entry *);
-static void		 cache_write_number(FILE *, unsigned int);
-static void		 cache_write_string(FILE *, const char *);
+static void		 cache_write_number(unsigned int);
+static void		 cache_write_string(const char *);
 
 RB_PROTOTYPE(cache_tree, cache_entry, entries, cache_cmp_entry)
 
 static struct cache_tree cache_tree = RB_INITIALIZER(cache_tree);
+static FILE		*cache_fp;
+static size_t		 cache_bufidx;
+static size_t		 cache_buflen;
+static size_t		 cache_bufsize;
 static int		 cache_modified;
+static char		*cache_buf;
 static char		*cache_file;
 
 RB_GENERATE(cache_tree, cache_entry, entries, cache_cmp_entry)
@@ -143,7 +143,8 @@ cache_get_metadata(struct track *t)
 	struct cache_entry *find, search;
 
 	search.path = t->path;
-	if ((find = RB_FIND(cache_tree, &cache_tree, &search)) == NULL)
+	find = RB_FIND(cache_tree, &cache_tree, &search);
+	if (find == NULL)
 		return -1;
 
 	t->album = find->album == NULL ? NULL : xstrdup(find->album);
@@ -166,32 +167,104 @@ cache_init(void)
 		msg_err("Cannot read metadata cache");
 }
 
-static void
-cache_read_entries(char *data, char *end)
+static int
+cache_read_field(char **field)
+{
+	size_t	 fieldlen;
+	char	*sep;
+
+	for (;;) {
+		if (cache_buflen < cache_bufsize) {
+			cache_buflen += fread(cache_buf + cache_buflen, 1,
+			    cache_bufsize - cache_buflen, cache_fp);
+			if (ferror(cache_fp)) {
+				LOG_ERR("fread");
+				return -1;
+			}
+			if (feof(cache_fp) && cache_buflen == 0)
+				return -1;
+		}
+
+		sep = memchr(cache_buf + cache_bufidx, '\0',
+		    cache_buflen - cache_bufidx);
+		if (sep != NULL)
+			break;
+
+		if (feof(cache_fp)) {
+			LOG_ERRX("no field separator at EOF");
+			return -1;
+		}
+
+		if (cache_bufidx > 0) {
+			(void)memmove(cache_buf, cache_buf + cache_bufidx,
+			    cache_buflen - cache_bufidx);
+			cache_buflen -= cache_bufidx;
+			cache_bufidx = 0;
+		} else {
+			if (SIZE_MAX - cache_bufsize < CACHE_BUFSIZE) {
+				LOG_ERRX("buffer size too large");
+				return -1;
+			}
+			cache_bufsize += CACHE_BUFSIZE;
+			cache_buf = xrealloc(cache_buf, cache_bufsize);
+		}
+	}
+
+	*field = cache_buf + cache_bufidx;
+	fieldlen = (sep - (cache_buf + cache_bufidx)) + 1;
+
+	if (cache_bufidx + fieldlen < cache_buflen)
+		cache_bufidx += fieldlen;
+	else {
+		cache_bufidx = 0;
+		cache_buflen = 0;
+	}
+
+	return 0;
+}
+
+static int
+cache_read_file(void)
 {
 	struct cache_entry	*e;
 	int			 ret;
 	unsigned int		 version;
 
-	if (cache_read_number(&data, end, &version) == -1)
-		return;
+	cache_fp = fopen(cache_file, "r");
+	if (cache_fp == NULL)
+		return -1;
+
+	cache_bufidx = 0;
+	cache_buflen = 0;
+	cache_bufsize = CACHE_BUFSIZE;
+	cache_buf = xmalloc(cache_bufsize);
+
+	if (cache_read_number(&version) == -1) {
+		free(cache_buf);
+		(void)fclose(cache_fp);
+		return -1;
+	}
 
 	if (version != CACHE_VERSION) {
 		LOG_ERRX("%u: unsupported metadata cache version", version);
-		return;
+		msg_errx("Unsupported metadata cache version");
+		free(cache_buf);
+		(void)fclose(cache_fp);
+		return -1;
 	}
 
 	for (;;) {
 		e = xmalloc(sizeof *e);
+
 		ret = 0;
-		ret += cache_read_string(&data, end, &e->path);
-		ret += cache_read_string(&data, end, &e->artist);
-		ret += cache_read_string(&data, end, &e->album);
-		ret += cache_read_string(&data, end, &e->date);
-		ret += cache_read_string(&data, end, &e->tracknumber);
-		ret += cache_read_string(&data, end, &e->title);
-		ret += cache_read_number(&data, end, &e->duration);
-		ret += cache_read_string(&data, end, &e->genre);
+		ret += cache_read_string(&e->path);
+		ret += cache_read_string(&e->artist);
+		ret += cache_read_string(&e->album);
+		ret += cache_read_string(&e->date);
+		ret += cache_read_string(&e->tracknumber);
+		ret += cache_read_string(&e->title);
+		ret += cache_read_number(&e->duration);
+		ret += cache_read_string(&e->genre);
 
 		/* Check for EOF or error. */
 		if (ret) {
@@ -201,70 +274,19 @@ cache_read_entries(char *data, char *end)
 
 		cache_add_entry(e);
 	}
-}
 
-static int
-cache_read_field(char **data, char *end, char **field)
-{
-	*field = *data;
-
-	/* Find end of field. */
-	while (*data < end)
-		if (*(*data)++ == '\0')
-			return 0;
-
-	/* EOF or field not NUL-terminated. */
-	return -1;
-}
-
-static int
-cache_read_file(void)
-{
-	struct stat	 sb;
-	int		 fd;
-	char		*data;
-
-	if ((fd = open(cache_file, O_RDONLY)) == -1) {
-		if (errno == ENOENT)
-			return 0;
-		else {
-			LOG_ERR("open: %s", cache_file);
-			return -1;
-		}
-	}
-
-	if (fstat(fd, &sb) == -1) {
-		LOG_ERR("fstat: %s", cache_file);
-		(void)close(fd);
-		return -1;
-	}
-
-	if (sb.st_size == 0) {
-		(void)close(fd);
-		return 0;
-	}
-
-	if ((data = mmap(0, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) ==
-	    MAP_FAILED) {
-		LOG_ERR("mmap: %s", cache_file);
-		(void)close(fd);
-		return -1;
-	}
-
-	cache_read_entries(data, data + sb.st_size);
-
-	(void)munmap(data, sb.st_size);
-	(void)close(fd);
+	free(cache_buf);
+	(void)fclose(cache_fp);
 	return 0;
 }
 
 static int
-cache_read_number(char **data, char *end, unsigned int *num)
+cache_read_number(unsigned int *num)
 {
 	char		*field;
 	const char	*errstr;
 
-	if (cache_read_field(data, end, &field) == -1)
+	if (cache_read_field(&field) == -1)
 		return -1;
 
 	*num = (unsigned int)strtonum(field, 0, UINT_MAX, &errstr);
@@ -275,11 +297,11 @@ cache_read_number(char **data, char *end, unsigned int *num)
 }
 
 static int
-cache_read_string(char **data, char *end, char **str)
+cache_read_string(char **str)
 {
 	char *field;
 
-	if (cache_read_field(data, end, &field) == -1) {
+	if (cache_read_field(&field) == -1) {
 		*str = NULL;
 		return -1;
 	}
@@ -302,41 +324,41 @@ cache_remove_entry(struct cache_entry *e)
 int
 cache_write_file(void)
 {
-	struct cache_entry	*e;
-	FILE			*fp;
+	struct cache_entry *e;
 
-	if ((fp = fopen(cache_file, "w")) == NULL) {
+	cache_fp = fopen(cache_file, "w");
+	if (cache_fp == NULL) {
 		LOG_ERR("fopen: %s", cache_file);
 		return -1;
 	}
 
-	cache_write_number(fp, CACHE_VERSION);
+	cache_write_number(CACHE_VERSION);
 	RB_FOREACH(e, cache_tree, &cache_tree) {
-		cache_write_string(fp, e->path);
-		cache_write_string(fp, e->artist);
-		cache_write_string(fp, e->album);
-		cache_write_string(fp, e->date);
-		cache_write_string(fp, e->tracknumber);
-		cache_write_string(fp, e->title);
-		cache_write_number(fp, e->duration);
-		cache_write_string(fp, e->genre);
+		cache_write_string(e->path);
+		cache_write_string(e->artist);
+		cache_write_string(e->album);
+		cache_write_string(e->date);
+		cache_write_string(e->tracknumber);
+		cache_write_string(e->title);
+		cache_write_number(e->duration);
+		cache_write_string(e->genre);
 	}
 
-	(void)fclose(fp);
+	(void)fclose(cache_fp);
 	cache_modified = 0;
 	return 0;
 }
 
 static void
-cache_write_number(FILE *fp, unsigned int num)
+cache_write_number(unsigned int num)
 {
-	(void)fprintf(fp, "%u%c", num, '\0');
+	(void)fprintf(cache_fp, "%u%c", num, '\0');
 }
 
 static void
-cache_write_string(FILE *fp, const char *str)
+cache_write_string(const char *str)
 {
 	if (str != NULL)
-		(void)fputs(str, fp);
-	(void)putc('\0', fp);
+		(void)fputs(str, cache_fp);
+	(void)putc('\0', cache_fp);
 }
