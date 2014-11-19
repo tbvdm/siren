f01b643dd0457086c14c8def886e054804bd663c
Tim van der Molen <tbvdm@xs4all.nl>
2014-11-19 15:51:42 +0100

    Better genre support.

--- ip/mpg123.c
+++ ip/mpg123.c
@@ -15,6 +15,7 @@
  */
 
 #include <fcntl.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -38,6 +39,210 @@ static int	 ip_mpg123_read(struct track *, int16_t *, size_t);
 static void	 ip_mpg123_seek(struct track *, unsigned int);
 
 static const char *ip_mpg123_extensions[] = { "mp1", "mp2", "mp3", NULL };
+
+static const char *ip_mpg123_genres[] = {
+	/*
+	 * ID3v1 genres (based on http://id3.org/id3v2.4.0-frames)
+	 */
+
+	"Blues",
+	"Classic Rock",
+	"Country",
+	"Dance",
+	"Disco",
+	"Funk",
+	"Grunge",
+	"Hip-Hop",
+	"Jazz",
+	"Metal",
+	"New Age",
+	"Oldies",
+	"Other",
+	"Pop",
+	"R&B",
+	"Rap",
+	"Reggae",
+	"Rock",
+	"Techno",
+	"Industrial",
+	"Alternative",
+	"Ska",
+	"Death Metal",
+	"Pranks",
+	"Soundtrack",
+	"Euro-Techno",
+	"Ambient",
+	"Trip-Hop",
+	"Vocal",
+	"Jazz+Funk",
+	"Fusion",
+	"Trance",
+	"Classical",
+	"Instrumental",
+	"Acid",
+	"House",
+	"Game",
+	"Sound Clip",
+	"Gospel",
+	"Noise",
+	"AlternRock",
+	"Bass",
+	"Soul",
+	"Punk",
+	"Space",
+	"Meditative",
+	"Instrumental Pop",
+	"Instrumental Rock",
+	"Ethnic",
+	"Gothic",
+	"Darkwave",
+	"Techno-Industrial",
+	"Electronic",
+	"Pop-Folk",
+	"Eurodance",
+	"Dream",
+	"Southern Rock",
+	"Comedy",
+	"Cult",
+	"Gangsta",
+	"Top 40",
+	"Christian Rap",
+	"Pop/Funk",
+	"Jungle",
+	"Native American",
+	"Cabaret",
+	"New Wave",
+	"Psychedelic",
+	"Rave",
+	"Showtunes",
+	"Trailer",
+	"Lo-Fi",
+	"Tribal",
+	"Acid Punk",
+	"Acid Jazz",
+	"Polka",
+	"Retro",
+	"Musical",
+	"Rock & Roll",
+	"Hard Rock",
+
+	/*
+	 * Winamp extensions (based on https://en.wikipedia.org/wiki/ID3)
+	 */
+
+	"Folk",
+	"Folk Rock",
+	"National Folk",
+	"Swing",
+	"Fast Fusion",
+	"Bebop",
+	"Latin",
+	"Revival",
+	"Celtic",
+	"Bluegrass",
+	"Avantgarde",
+	"Gothic Rock",
+	"Progressive Rock",
+	"Psychedelic Rock",
+	"Symphonic Rock",
+	"Slow Rock",
+	"Big Band",
+	"Chorus",
+	"Easy Listening",
+	"Acoustic",
+	"Humour",
+	"Speech",
+	"Chanson",
+	"Opera",
+	"Chamber Music",
+	"Sonata",
+	"Symphony",
+	"Booty Bass",
+	"Primus",
+	"Porn Groove",
+	"Satire",
+	"Slow Jam",
+	"Club",
+	"Tango",
+	"Samba",
+	"Folklore",
+	"Ballad",
+	"Power Ballad",
+	"Rhythmic Soul",
+	"Freestyle",
+	"Duet",
+	"Punk Rock",
+	"Drum Solo",
+	"A Cappella",
+	"Euro-House",
+	"Dance Hall",
+	"Goa",
+	"Drum & Bass",
+	"Club-House",
+	"Hardcore",
+	"Terror",
+	"Indie",
+	"Britpop",
+	"Afro-punk",
+	"Polsk Punk",
+	"Beat",
+	"Christian Gangsta Rap",
+	"Heavy Metal",
+	"Black Metal",
+	"Crossover",
+	"Contemporary Christian",
+	"Christian Rock",
+	"Merengue",
+	"Salsa",
+	"Thrash Metal",
+	"Anime",
+	"Jpop",
+	"Synthpop",
+	"Abstract",
+	"Art Rock",
+	"Baroque",
+	"Bhangra",
+	"Big Beat",
+	"Breakbeat",
+	"Chillout",
+	"Downtempo",
+	"Dub",
+	"EBM",
+	"Eclectic",
+	"Electro",
+	"Electroclash",
+	"Emo",
+	"Experimental",
+	"Garage",
+	"Global",
+	"IDM",
+	"Illbient",
+	"Industro-Goth",
+	"Jam Band",
+	"Krautrock",
+	"Leftfield",
+	"Lounge",
+	"Math Rock",
+	"New Romantic",
+	"Nu-Breakz",
+	"Post-Punk",
+	"Post-Rock",
+	"Psytrance",
+	"Shoegaze",
+	"Space Rock",
+	"Trop Rock",
+	"World Music",
+	"Neoclassical",
+	"Audiobook",
+	"Audio Theatre",
+	"Neue Deutsche Welle",
+	"Podcast",
+	"Indie Rock",
+	"G-Funk",
+	"Dubstep",
+	"Garage Rock",
+	"Psybient"
+};
 
 const struct ip	 ip = {
 	"mpg123",
@@ -67,6 +272,32 @@ ip_mpg123_close_fd_handle(int fd, mpg123_handle *hdl)
 	mpg123_close(hdl);
 	mpg123_delete(hdl);
 	close(fd);
+}
+
+static char *
+ip_mpg123_get_genre(mpg123_string *s)
+{
+	int		 idx;
+	char		*t;
+	const char	*errstr;
+
+	/*
+	 * If the TCON (genre) frame is of the form "n" or "(n)" where "n" is
+	 * a numerical string, then assume "n" is an ID3v1 genre index.
+	 */
+
+	if (s->p[0] != '(')
+		t = s->p;
+	else {
+		t = s->p + 1;
+		t[strcspn(t, ")")] = '\0';
+	}
+
+	idx = strtonum(t, 0, NELEMENTS(ip_mpg123_genres) - 1, &errstr);
+	if (errstr == NULL)
+		return xstrdup(ip_mpg123_genres[idx]);
+	else
+		return xstrdup(s->p);
 }
 
 static int
@@ -117,7 +348,8 @@ ip_mpg123_get_metadata(struct track *t)
 			if (!strncmp(v2->text[i].id, "TDRC", 4))
 				t->date = xstrdup(v2->text[i].text.p);
 			if (!strncmp(v2->text[i].id, "TCON", 4))
-				t->genre = xstrdup(v2->text[i].text.p);
+				t->genre =
+				    ip_mpg123_get_genre(&v2->text[i].text);
 			if (!strncmp(v2->text[i].id, "TIT2", 4))
 				t->title = xstrdup(v2->text[i].text.p);
 			if (!strncmp(v2->text[i].id, "TRCK", 4)) {
@@ -135,7 +367,9 @@ ip_mpg123_get_metadata(struct track *t)
 		t->artist = xstrndup(v1->artist, sizeof v1->artist);
 		t->date = xstrndup(v1->year, sizeof v1->year);
 		t->title = xstrndup(v1->title, sizeof v1->title);
-		t->genre = NULL;
+
+		if (v1->genre < NELEMENTS(ip_mpg123_genres))
+			t->genre = xstrdup(ip_mpg123_genres[v1->genre]);
 
 		/*
 		 * ID3v1.1 allows the track number to be stored in the comment
