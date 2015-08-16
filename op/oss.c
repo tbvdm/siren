e7ae1584d6781cd86eb67288c0e1d9dfa1a7e460
Tim van der Molen <tim@kariliq.nl>
2015-08-16 23:05:03 +0200

    Essentially revert commit d7bc6798ffdb.
    OSS on FreeBSD resets the volume when the device is opened. Since the oss
    plug-in closes the device after playback has finished (so that other programs
    can use it), the volume is reset whenever a new track is played back. This is
    very annoying, so ensure the original volume is set when the device is opened.
    
    The drawback is that volume changes by other programs (e.g. mixer programs) may
    be lost, but that is far less annoying.

--- op/oss.c
+++ op/oss.c
@@ -164,8 +164,11 @@ op_oss_set_volume(unsigned int volume)
 	int arg;
 
 	if (op_oss_fd == -1)
-		msg_errx("Cannot change the volume level while the device is "
-		    "closed");
+		/*
+		 * The device is closed. The volume will be set when the device
+		 * is opened.
+		 */
+		op_oss_volume = volume;
 	else {
 		/* Set the volume level for the left and right channels. */
 		arg = volume | (volume << 8);
@@ -265,6 +268,11 @@ op_oss_start(struct sample_format *sf)
 		op_oss_buffer_size = OP_OSS_BUFSIZE;
 	} else
 		op_oss_buffer_size = arg;
+
+#ifdef OP_OSS_HAVE_VOLUME_SUPPORT
+	if (op_oss_volume != -1)
+		op_oss_set_volume(op_oss_volume);
+#endif
 
 	return 0;
 
